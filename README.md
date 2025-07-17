334c95b1d2c0059ec3c62d3d366aedde46075d7c
brianjones <brianpjoness@gmail.com>
2025-07-16 21:41:04 -0500

    This is me figuring out how to use git

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # This is README
+
+Creating a linear regression model from scratch
+The goal is to minimize the error 
+
+
+Error Function: Mean Squared Error
+
+E = (1/n) * sum (i -> n) (yi - (mxi +b))^2
+Average value of the difference between actual and predicted values squared
+
+
+How we will minimize E:
+y = mx + b
+We can change m and b, so we take partial derivative with respect to m and b
+Allows you to find the direction of the steepest ascent (with partial derivative) so you take opposite direction
+
+m = m - L (dE/dm)
+b = b - L (dE/db)
+
+L is the learning rate that determines how quickly it will come to a solution, lower learning rate is less error
--- main.py
+++ main.py
@@ -1,2 +1,61 @@
 
-print("Hello Git")
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+np.random.seed(0)
+
+mean = [10, 70]
+
+# covariance matrix
+cov = [[1, .95], [.95, 2]]
+
+data = np.random.multivariate_normal(mean, cov, size=100)
+
+df = pd.DataFrame(data, columns=["Hours Studied", "Test Score"])
+
+# print(df)
+# print(df.corr())
+# plt.scatter(df["Hours Studied"], df["Test Score"])
+# plt.show()
+
+def mean_squared_error(m, b, points):  #  this is to calculate loss function
+    total_error = 0
+
+    for i in range(len(points)):
+        x = points.iloc[i]["Hours Studied"]
+        y = points.iloc[i]["Test Score"]
+        total_error += (y - (m * x + b)) **2
+    total_error /= float(len(points))
+
+def gradient_descent(m_now, b_now, points, L):
+    m_gradient = 0
+    b_gradient = 0
+
+    n = len(points)  # number of points
+
+    for i in range(n):
+        x = points.iloc[i]["Hours Studied"]
+        y = points.iloc[i]["Test Score"]
+
+        m_gradient += -(2/n) * x * (y-(m_now * x + b_now))    # these are the partial derivatives (peak ascending direction)
+        b_gradient += -(2/n) * (y-(m_now * x + b_now))
+
+    m = m_now - m_gradient * L
+    b = b_now - b_gradient * L
+    return m, b
+
+m = 0
+b = 0
+L = 0.001 # learning rate
+epochs = 1000 # training iterations
+
+for i in range(epochs):
+    m, b = gradient_descent(m, b, df, L)
+    if i % 50 ==0:
+        print(i)
+print(m, b)
+
+plt.scatter(df["Hours Studied"], df["Test Score"])
+plt.plot(list(range(6, 12)), [ m * x + b for x in range(6, 12)], color = "red")
+plt.show()
